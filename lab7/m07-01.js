b6c10e2cb8afac34798e3decf917ef689a4a1014
Jenia <chernikjenia1999@gmail.com>
2021-10-07 20:18:34 +0300

    lab_7 was edited

--- lab7/m07-01.js
+++ lab7/m07-01.js
@@ -1,42 +1,5 @@
 const fs = require("fs");
 
-// function StaticFiles(staticPath) {
-//     this.staticPath = staticPath;
-//
-//     this.createPath = (fileName) => this.staticPath + fileName;
-//
-//     this.sendFile = (req, resp, headers) => {
-//         let fileName = this.createPath(req.url);
-//
-//         fs.access(fileName, (err) => {
-//             if(err) {
-//                 this.HTTP_404(req, resp);
-//             }
-//             else {
-//                 resp.writeHead(200, headers);
-//                 fs.createReadStream(fileName).pipe(resp);
-//             }
-//         });
-//     };
-//
-//     this.HTTP_405 = (req, resp) => {
-//         resp.writeHead(405, { "Content-Type":"text/html"});
-//         resp.end("<h1>Error 405: Method not allowed</h1>");
-//     };
-//
-//     this.HTTP_404 = (req, resp) => {
-//         resp.writeHead(404, { "Content-Type": "text/html" });
-//         resp.end("<h1>Error 404: Resource not found</h1>");
-//     };
-//
-//     this.isStatic =  (ext, fileName) => {
-//         let reg = new RegExp(`^\/.+\.${ext}$`);
-//         return reg.test(fileName);
-//     };
-// }
-//
-// module.exports = staticPath => new StaticFiles(staticPath);
-
 class StaticFiles {
     constructor(staticPath) {
         this.staticPath = staticPath;
